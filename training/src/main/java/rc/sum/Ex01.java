f9df56022ff23c58f659af6dcb6bf317897d5f30
weichou99 <weichou99@gmail.com>
2019-01-31 10:20:30 +0800

    add quz

--- training/src/main/java/rc/sum/Ex01.java
+++ training/src/main/java/rc/sum/Ex01.java
@@ -2,7 +2,11 @@ package rc.sum;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.xml.crypto.dsig.spec.HMACParameterSpec;
 
 import rc.bean.Product;
 
@@ -48,6 +52,29 @@ public class Ex01 {
 		list.add(new Product("Book", new BigDecimal("39.99")));
 		list.add(new Product("iPhone", new BigDecimal("300.55")));
 		list.add(new Product("DVD", new BigDecimal("9.33")));
+		
+	}
+	
+	public void quz5() {
+		List<Map<String, Object>> list = new ArrayList<>();
+		
+		Map<String, Object> map1 = new HashMap<>();
+		map1.put("name", "Book");
+		map1.put("price", new BigDecimal("39.99"));
+		
+		Map<String, Object> map2 = new HashMap<>();
+		map2.put("name", "iPhone");
+		map2.put("price", new BigDecimal("300.55"));
+		
+		Map<String, Object> map3 = new HashMap<>();
+		map3.put("name", "DVD");
+		map3.put("price", new BigDecimal("9.33"));
+		
+		list.add(map1);
+		list.add(map2);
+		list.add(map3);
+		
+		
 		
 	}
 
